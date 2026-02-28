1229fdea1b89a11590067223225662150156b9e8
giuseppegabrieleciulla <giuseppeciulladev@gmail.com>
2026-02-28 22:15:59 +0100

    fix addcmul warning

--- DeBERTa/optims/xadam.py
+++ DeBERTa/optims/xadam.py
@@ -47,7 +47,7 @@ def adamw(data,
     torch.max(next_v.mul_(beta2), grad.abs().to(next_v), out=next_v)
     update = next_m/(next_v+eps)
   else:
-    next_v.mul_(beta2).addcmul_(beta2_, grad, grad)
+    next_v.mul_(beta2).addcmul_(grad, grad, value=beta2_)
     if eps_mode == 0:
       update = (next_m)*(next_v+eps).rsqrt()
     elif eps_mode == 1:
